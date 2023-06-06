557c6e34fd9f0cb0b50c4ff0c7c754f781d5fb0b
Abelliuxl <35425033+Abelliuxl@users.noreply.github.com>
2023-06-06 13:24:39 +0800

    Update my_multi.js

--- my_multi.js
+++ my_multi.js
@@ -1,3 +1,3 @@
 const style = document.createElement('style');
-style.textContent = 'html { transform: scale(0.9); }';
+style.textContent = 'html { transform: scale(0.3); }';
 document.head.appendChild(style);
